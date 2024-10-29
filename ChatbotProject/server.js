3137d3e70e2f141f5386370e387505f675ec20c3
ashantib102 <44121027+ashantib102@users.noreply.github.com>
2024-10-29 06:59:51 -0400

    Turned into react.js chat running on node.js server

---
+++ ChatbotProject/server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    // Determine file path based on the request URL
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+
+    // Determine content type based on file extension
+    let extname = path.extname(filePath);
+    let contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+    }
+
+    // Read and serve the file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
